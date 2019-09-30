fc74d6f309e4ee594a8a810604422597a0768eea
Keal Jones <keal.jones@workiva.com>
2019-09-30 13:16:32 -0700

    add submodule update command for new clones

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
+git submodule update --init
 cd docs_experience && pub get && pub run build_runner build -o ./latest -r && cd ..
 cd ss_experience && pub get && pub run build_runner build -o ./latest -r && cd ..
 cd shell && pub get && webdev serve
